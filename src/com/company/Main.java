e0b48d622341c73c40ef159cac6e3a9551d32db5
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-29 13:55:23 -0500

    Create two new String objects to compare.

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	String myName = new String("Mikayla Clemons");
+
+	String myNameLowerCase = new String("mikayla clemons");
+
+	boolean isEqual = myName.equals(myNameLowerCase);
+
+        System.out.println(isEqual);
+
+    boolean areEqual = myName.equalsIgnoreCase(myNameLowerCase);
+        System.out.println(areEqual);
+    }
+}
